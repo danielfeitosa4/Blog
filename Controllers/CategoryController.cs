426257e97cd3f63e6976eae718d5f538501fba6b
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-09-23 10:11:37 -0300

    Create Repository

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("category")]
+    public class CategoryController : ControllerBase
+    {
+
+    }
+}
