cd1748b1cb4decd2214214e7b4d69d80177ed599
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-09-24 09:50:23 -0300

    escopo autenticacao

---
+++ Configuration.cs
@@ -0,0 +1,8 @@
+namespace Blog
+{
+    public static class Configuration
+    {
+        // TOKEN - JWT - Json Web Token
+        public static string JwtKey { get; set; } = "U0RMR1ZLSUtITkFTREJJS0xITjIzNDUyNVNERlJPSEpBV1NFUkhPUMOHR0laREZLTE5ETENWMzQ1NjdLV1NKTkZMQVdFSUtHTkFTT0zDh1ZJS1pITlNEWExWSUtBU0RFUk4yMzQ1MjM0MDU=";
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Blog.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            
+        }
+    }
+}
