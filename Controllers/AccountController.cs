881f1e4f9c11817d895f9ca4b9a7e1635de0e703
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-09-24 21:03:27 -0300

    Hash de senha

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,12 @@
+using System.Data.Common;
+using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
 using Blog.Services;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
@@ -12,7 +19,43 @@ namespace Blog.Controllers
             _tokenService = tokenService;
         }
 
-        [HttpPost("v1/login")]
+        [HttpPost("v1/accounts")]
+        public async Task<IActionResult> Post(RegisterViewModel model, BlogDataContext context)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(length: 25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email, password
+                }));
+            }
+            catch(DbUpdateException)
+            {
+                return StatusCode(400, new ResultViewModel<string>("05x99 - Este e-mail já está cadastrado"));
+            }
+            catch
+            {
+                return StatusCode(500, "05x04 - Falha interna no servidor");
+            }
+        }
+
+        [HttpPost("v1/accounts/login")]
         public IActionResult Login()
         {
             var token = _tokenService.GenerateToken(null);
